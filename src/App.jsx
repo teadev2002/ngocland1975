cb14f62f03f0a9e414b29562834b38073625d38d
teadev2002 <anhhoanggg855@gmail.com>
2025-06-16 13:17:42 +0700

    check

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more 123 edit
+        Click on the Vite and React logos to learn more 123 edit 1
       </p>
     </>
   )
