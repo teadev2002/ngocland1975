87d162cac71c1828e2e03b79ba8aeb1f94b168dd
teadev2002 <anhhoanggg855@gmail.com>
2025-06-17 12:51:14 +0700

    navigate /home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
  import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/HomePages/Home';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   )
